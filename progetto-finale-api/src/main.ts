66a9d37a7a5a44129dcaf117e99ce9f244beda70
Mondrian92 <m.maugeri92@gmail.com>
2021-02-13 17:43:40 +0100

    Adding imports

---
+++ progetto-finale-api/src/main.ts
@@ -0,0 +1,7 @@
+import { OpenWeatherMapApi} from 'node-ts-open-weather-map'
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import fs from "fs";
+import axios from 'axios';
+

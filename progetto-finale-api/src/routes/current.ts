2c0ce29a67622ce8a0fee389d0c1371181b34ac7
BuildTools <unconfigured@null.spigotmc.org>
2021-02-14 16:50:27 +0100

    Added validator

--- progetto-finale-api/src/routes/current.ts
+++ progetto-finale-api/src/routes/current.ts
@@ -1,46 +1,71 @@
-import express, {Request, Response, NextFunction} from "express";
-import OpenWeatherMap from 'openweathermap-ts';
-import {weather} from '../main'
+import express, { Request, Response, NextFunction } from "express";
+import { weather } from "../main";
+import { param, query } from "express-validator";
+import { validationHandler } from "../validation";
 //const weather = new OpenWeatherMap({apiKey: '340269648576eaaff42a70eb35325e3a'});
 const router = express.Router();
 
-router.get('/cities/:cityName', async ({ params: {cityName}} : Request, res: Response) => {
+router.get(
+  "/cities/:cityName",
+  param("cityName").isString(),
+  validationHandler,
+  async ({ params: { cityName } }: Request, res: Response) => {
     try {
-        console.log("sono in cities");
-        console.log("Params", cityName);
-        
-        const wea = await weather.getCurrentWeatherByCityName({cityName});
-        res.status(200).json({wea})
-    } catch (error) {
-        res.status(400).json({message: "Error", errorType: error})
-    }
-})
+      console.log("sono in cities");
+      console.log("Params", cityName);
 
-router.get('/id/:cityId', async ({ params: {cityId}} : Request, res: Response) => {
+      const wea = await weather.getCurrentWeatherByCityName({ cityName });
+      res.status(200).json({ wea });
+    } catch (error) {
+      res.status(400).json({ message: "Error", errorType: error });
+    }
+  }
+);
+
+router.get(
+  "/id/:cityId",
+  param("cityId").isNumeric(),
+  validationHandler,
+  async ({ params: { cityId } }: Request, res: Response) => {
     try {
-        const wea = await weather.getCurrentWeatherByCityId(Number(cityId));
-        res.status(200).json({wea})
+      const wea = await weather.getCurrentWeatherByCityId(Number(cityId));
+      res.status(200).json({ wea });
     } catch (error) {
-        res.status(400).json({message: "Error", errorType: error})
+      res.status(400).json({ message: "Error", errorType: error });
     }
-})
+  }
+);
 
-router.get('/zipcodes/:zipCode', async ({ params: {zipCode}} : Request, res: Response) => {
+router.get(
+  "/zipcodes/:zipCode",
+  param("zipCode").isNumeric(),
+  validationHandler,
+  async ({ params: { zipCode } }: Request, res: Response) => {
     try {
-        const wea = await weather.getCurrentWeatherByZipcode(Number(zipCode));
-        res.status(200).json({wea})
+      const wea = await weather.getCurrentWeatherByZipcode(Number(zipCode));
+      res.status(200).json({ wea });
     } catch (error) {
-        res.status(400).json({message: "Error", errorType: error})
+      res.status(400).json({ message: "Error", errorType: error });
     }
-})
+  }
+);
 
-router.get('/', async ({ query: {long, lat}} : Request, res: Response) => {
+router.get(
+  "/coordinates",
+  query("long").isNumeric(),
+  query("lat").isNumeric(),
+  validationHandler,
+  async ({ query: { long, lat } }: Request, res: Response) => {
     try {
-        const wea = await weather.getCurrentWeatherByGeoCoordinates(Number(lat), Number(long));
-        res.status(200).json({wea})
+      const wea = await weather.getCurrentWeatherByGeoCoordinates(
+        Number(lat),
+        Number(long)
+      );
+      res.status(200).json({ wea });
     } catch (error) {
-        res.status(400).json({message: "Error", errorType: error})
+      res.status(400).json({ message: "Error", errorType: error });
     }
-})
+  }
+);
 
-export { router as current }
+export { router as current };
--- progetto-finale-api/src/routes/forecast.ts
+++ progetto-finale-api/src/routes/forecast.ts
@@ -1,43 +1,68 @@
-import express, {Request, Response, NextFunction} from "express";
-import OpenWeatherMap from 'openweathermap-ts';
-import {weather} from '../main'
+import express, { Request, Response, NextFunction } from "express";
+import { weather } from "../main";
+import { param, query } from "express-validator";
+import { validationHandler } from "../validation";
 //const weather = new OpenWeatherMap({apiKey: '340269648576eaaff42a70eb35325e3a'});
 const router = express.Router();
 
-router.get('/cities/:cityName', async ({ params: {cityName}} : Request, res: Response) => {
+router.get(
+  "/cities/:cityName",
+  param("cityName").isString(),
+  validationHandler,
+  async ({ params: { cityName } }: Request, res: Response) => {
     try {
-        const wea = await weather.getThreeHourForecastByCityName({cityName});
-        res.status(200).json({wea})
+      const wea = await weather.getThreeHourForecastByCityName({ cityName });
+      res.status(200).json({ wea });
     } catch (error) {
-        res.status(400).json({message: "Error", errorType: error})
+      res.status(400).json({ message: "Error", errorType: error });
     }
-})
+  }
+);
 
-router.get('/id/:cityId', async ({ params: {cityId}} : Request, res: Response) => {
+router.get(
+  "/id/:cityId",
+  param("cityId").isNumeric(),
+  validationHandler,
+  async ({ params: { cityId } }: Request, res: Response) => {
     try {
-        const wea = await weather.getThreeHourForecastByCityId(Number(cityId));
-        res.status(200).json({wea})
+      const wea = await weather.getThreeHourForecastByCityId(Number(cityId));
+      res.status(200).json({ wea });
     } catch (error) {
-        res.status(400).json({message: "Error", errorType: error})
+      res.status(400).json({ message: "Error", errorType: error });
     }
-})
+  }
+);
 
-router.get('/zipcodes/:zipCode', async ({ params: {zipCode}} : Request, res: Response) => {
+router.get(
+  "/zipcodes/:zipCode",
+  param("zipCode").isNumeric(),
+  validationHandler,
+  async ({ params: { zipCode } }: Request, res: Response) => {
     try {
-        const wea = await weather.getThreeHourForecastByZipcode(Number(zipCode));
-        res.status(200).json({wea})
+      const wea = await weather.getThreeHourForecastByZipcode(Number(zipCode));
+      res.status(200).json({ wea });
     } catch (error) {
-        res.status(400).json({message: "Error", errorType: error})
+      res.status(400).json({ message: "Error", errorType: error });
     }
-})
+  }
+);
 
-router.get('/coordinates', async ({ query: {long, lat}} : Request, res: Response) => {
+router.get(
+  "/coordinates",
+  query("long").isNumeric(),
+  query("lat").isNumeric(),
+  validationHandler,
+  async ({ query: { long, lat } }: Request, res: Response) => {
     try {
-        const wea = await weather.getThreeHourForecastByGeoCoordinates(Number(lat), Number(long));
-        res.status(200).json({wea})
+      const wea = await weather.getThreeHourForecastByGeoCoordinates(
+        Number(lat),
+        Number(long)
+      );
+      res.status(200).json({ wea });
     } catch (error) {
-        res.status(400).json({message: "Error", errorType: error})
+      res.status(400).json({ message: "Error", errorType: error });
     }
-})
+  }
+);
 
-export { router as forecast }
+export { router as forecast };

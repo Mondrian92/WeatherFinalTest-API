169ee6e7a31b7cd0487e24a7f4b133d8593bb68d
VincenzoZimbone <vincenzo.zimbone@gmail.com>
2021-02-18 17:30:38 +0100

    Adding API in api-caller.service

--- weather-angular/src/app/services/api-caller.service.ts
+++ weather-angular/src/app/services/api-caller.service.ts
@@ -15,6 +15,14 @@ export class ApiCallerService {
   constructor(private client:HttpClient) {}
   private uri = "http://localhost:3001/auth/" 
   
+  //const uri = "http://localhost:3001/auth/login";
+
+  //const uriCurrent = "http://localhost:3001/weathers/current";
+  
+  //const uriForecast = "http://localhost:3001/weathers/forecast";
+
+
+  
   login = async ( email:string, password:string ) =>{
     const headers = new HttpHeaders().set( "email", email ).set( "password", password );
     const response = await this.client.get(this.uri+"login", {headers}).toPromise();
@@ -25,6 +33,27 @@ export class ApiCallerService {
 
   isLogged = () => {
     return this.client.get("http://localhost:3001/checkLogin/").toPromise()
-  }
+  }  
+  
+    // CURRRENT
+
+    //const header= new HttpHeaders().set("unit",unit);
+    //return await this.client.get(this.uriCurrent+"/cities/:cityName",{cityName, header}).toPromise();
+    //}
+  
+    //FORECAST
+    
+    //forecastCityName = async (cityName: string, unit: string) => {
+    //const header= new HttpHeaders().set("unit",unit);
+    //return await this.client.get(this.uriForecast+"/cities/:cityName",{cityName, header}).toPromise();
+    //}
+  
+  
+    //logout = async {
+    // const headers = new HttpHeaders().set();
+    // return const response = await this.client.get(this.uri +"logout",{headers}).toPromise();
+    //}
+
+
   
 }

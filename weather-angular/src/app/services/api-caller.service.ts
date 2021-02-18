3d182bf4c2a1c2e60faf5971eb777aa8f46cbe56
VincenzoZimbone <vincenzo.zimbone@gmail.com>
2021-02-18 20:40:09 +0100

    Add API in service

--- weather-angular/src/app/services/api-caller.service.ts
+++ weather-angular/src/app/services/api-caller.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { HttpHeaders } from '@angular/common/http';
-import { LoginRes } from '../interfaces/login-res'
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,63 +8,112 @@ import { LoginRes } from '../interfaces/login-res'
 
 export class ApiCallerService {
 
-  constructor(private client:HttpClient) {}
-  private uri = "http://localhost:3001/auth/" 
-<<<<<<< HEAD
-  private uriIsLogged = "http://localhost:3001/checkLogin/"
-  //const uri = "http://localhost:3001/auth/login";
-=======
-  
-  const uri = "http://localhost:3001/auth/login";
->>>>>>> 0dcd8245f1fc35e9fef76e70a1ea9882acd7f56e
+  constructor(private client: HttpClient) { }
+  private uri = "http://localhost:3001/auth/"
 
-  const uriCurrent = "http://localhost:3001/weathers/current";
-  
-  const uriForecast = "http://localhost:3001/weathers/forecast";
+  private uriLogin = "http://localhost:3001/auth/login";
+
+  private uriCurrent = "http://localhost:3001/weathers/current";
+
+  private uriForecast = "http://localhost:3001/weathers/forecast";
+
+  private uriUpdates = "http://localhost:3001/updates";
 
 
-  
-  login = async ( email:string, password:string ) =>{
-    const headers = new HttpHeaders().set( "email", email ).set( "password", password );
-    return await this.client.get(this.uri+"login", {headers}).toPromise().then((res: LoginRes) => {
-      sessionStorage.setItem("user", JSON.stringify({email, token: res.token}))
-    });
+
+  login = async (email: string, password: string) => {
+    const headers = new HttpHeaders().set("email", email).set("password", password);
+    const response = await this.client.get(this.uriLogin + "login", { headers }).toPromise();
   }
 
-  register = async ( name:string, surname:string, username:string, email:string, password:string, country:string, city:string, unit:string ) =>
-  await this.client.post(this.uri+"register",{name, surname, username, email, password, country, city, unit}).toPromise();
+  register = async (name: string, surname: string, username: string, email: string, password: string, country: string, city: string, unit: string) =>
+    await this.client.post(this.uri + "register", { name, surname, username, email, password, country, city, unit }).toPromise();
 
-  logout = async () =>{
-    const { token } = JSON.parse(sessionStorage.getItem("user"))
-    const headers = new HttpHeaders().set("token",token);
-    await this.client.delete(this.uri+"logout", { headers } )
+  isLogged = () => {
+    return this.client.get("http://localhost:3001/checkLogin/").toPromise()
   }
 
-  isLogged = (): Promise<LoginRes> => {
-    const { token } = JSON.parse(sessionStorage.getItem("user"))
-    const headers = new HttpHeaders().set("token",token);
-    return this.client.get(this.uriIsLogged, { headers } ).toPromise() as Promise<LoginRes>
-  }  
-  
-    // CURRRENT
-
-    const header= new HttpHeaders().set("unit",unit);
-    return await this.client.get(this.uriCurrent+"/cities/:cityName",{cityName, header}).toPromise();
-    }
-  
-    //FORECAST
-    
-    forecastCityName = async (cityName: string, unit: string) => {
-    const header= new HttpHeaders().set("unit",unit);
-    return await this.client.get(this.uriForecast+"/cities/:cityName",{cityName, header}).toPromise();
-    }
-  
-  
-    logout = async () {
-     const headers = new HttpHeaders().set();
-     return const response = await this.client.get(this.uri +"logout",{headers}).toPromise();
-    }
+  logout = async (token: string) => {
+    const headers = new HttpHeaders().set("token", token);
+    return await this.client.get(this.uri + "logout", { headers }).toPromise();
+  }
 
 
-  
+
+  //CURRENT  
+  currentCityName = async (cityName: string, unit: string) => {
+    const headers = new HttpHeaders().set("unit", unit);
+    const params = new HttpParams().set("city", cityName);
+    return await this.client.get(this.uriCurrent + "/cities/:cityName", { headers, params }).toPromise();
+  }
+
+  currentCityId = async (cityId: string, unit: string) => {
+    const headers = new HttpHeaders().set("unit", unit);
+    const params = new HttpParams().set("cityId", cityId);
+    return await this.client.get(this.uriCurrent + "/id/:cityId", { headers, params }).toPromise();
+  }
+
+  currentZipCode = async (zipCode: string, countryCode: string, unit: string) => {
+    const headers = new HttpHeaders().set("unit", unit);
+    const params = new HttpParams();
+    params.append("zipCode", zipCode);
+    params.append("countryCode", countryCode)
+    return await this.client.get(this.uriCurrent + "/coutries/:countryCode/zipcodes/:zipCode", { headers, params }).toPromise();
+  }
+
+
+  currentCoordinates = async (long: string, lat: string, unit: string) => {
+    const headers = new HttpHeaders().set("unit", unit);
+    return await this.client.get(this.uriCurrent + `/coordinates?long=${long}&lat=${lat}`, { headers }).toPromise();
+  }
+
+
+  //FORECAST
+  forecastCityName = async (cityName: string, unit: string) => {
+    const headers = new HttpHeaders().set("unit", unit);
+    const params = new HttpParams().set("city", cityName);
+    return await this.client.get(this.uriForecast + "/cities/:cityName", { headers, params }).toPromise();
+  }
+
+
+  forecastCityId = async (cityId: string, unit: string) => {
+    const headers = new HttpHeaders().set("unit", unit);
+    const params = new HttpParams().set("cityId", cityId);
+    return await this.client.get(this.uriForecast + "/id/:cityId", { headers, params }).toPromise();
+  }
+
+  forecastZipCode = async (zipCode: string, countryCode: string, unit: string) => {
+    const headers = new HttpHeaders().set("unit", unit);
+    const params = new HttpParams();
+    params.append("zipCode", zipCode);
+    params.append("countryCode", countryCode)
+    return await this.client.get(this.uriForecast + "/coutries/:countryCode/zipcodes/:zipCode", { headers, params }).toPromise();
+  }
+
+
+  forecastCoordinates = async (long: string, lat: string, unit: string) => {
+    const headers = new HttpHeaders().set("unit", unit);
+    return await this.client.get(this.uriCurrent + `/coordinates?long=${long}&lat=${lat}`, { headers }).toPromise();
+  }
+
+
+  //UPDATES
+  updateUsername = async (username: string) => {
+    await this.client.put(this.uriUpdates + "/username", { username }).toPromise();
+  }
+
+  updateCity = async (city: string) => {
+    await this.client.put(this.uriUpdates + "/city", { city }).toPromise();
+  }
+
+  updateCountry = async (country: string) => {
+    await this.client.put(this.uriUpdates + "/country", { country }).toPromise();
+  }
+
+  updateUnit = async (unit: string) => {
+    await this.client.put(this.uriUpdates + "/unit", { unit }).toPromise();
+  }
+
+
+
 }
